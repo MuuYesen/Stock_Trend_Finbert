0408f28a466c863acdb0585f55288ec957f258f5
SuperMan0010 <1320563812@qq.com>
2020-12-26 22:59:45 +0800

    New Release
    New Release

---
+++ finbert/figure.py
@@ -0,0 +1,132 @@
+﻿#!/usr/bin/env python
+# coding: utf-8
+
+import torch as t
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler,TensorDataset, random_split
+
+
+class InputExample(object):
+    def __init__(self, text, label):
+        self.text = text
+        self.label = label
+
+
+class InputFeature(object):
+    def __init__(self,input_id,attention_mask,label_id):
+        self.input_id = input_id
+        self.attention_mask = attention_mask
+        self.label_id = label_id
+
+class Tool(object):
+
+    @classmethod
+    def get_weight_from_dataset(cls,dataset,lable_ids_list,class_weight):
+        if class_weight == 'balanced':
+            label_list = t.tensor([dataset[i][2] for i in range(dataset.__len__())]).numpy()
+            weight = t.tensor([label_list.size/label_list[label_list == label].size for label in list(lable_ids_list)])
+        else:
+            weight = t.ones(1,len(lable_ids_list))
+        return weight
+
+
+class Config(object): ### 存储用户指定的要求 数据类
+    def __init__(
+                self,
+                label_map,
+                max_seq_length,
+                train_batch_size,
+                eval_batch_size,
+                learning_rate,
+                num_train_epochs,
+                warm_up_proportion,
+                no_cuda,
+                do_lower_case,
+                seed,
+                local_rank,
+                gradient_accumulation_steps,
+                fp16,
+                discriminate,
+                gradual_unfreeze,
+                encoder_no,
+                dft_rate
+                ):
+        self.label_map = label_map
+        self.do_lower_case = do_lower_case
+        self.max_seq_length = max_seq_length
+        self.train_batch_size = train_batch_size
+        self.local_rank = local_rank
+        self.eval_batch_size = eval_batch_size
+        self.learning_rate = learning_rate
+        self.num_train_epochs = num_train_epochs
+        self.warm_up_proportion = warm_up_proportion
+        self.no_cuda = no_cuda
+        self.seed = seed
+        self.gradient_accumulation_steps = gradient_accumulation_steps
+        self.fp16 = fp16
+        self.discriminate = discriminate
+        self.gradual_unfreeze = gradual_unfreeze
+        self.encoder_no = encoder_no
+        self.dft_rate = dft_rate
+
+
+class FinSentProcessor(object):  ### 假设数据已经分好句了  模板拼接一致，但逻辑改变
+
+    def convert_raws2examples(self,raw_data):
+        examples = []
+        for text,label in raw_data:
+            examples.append(InputExample(text,label))
+        return examples
+
+    def convert_examples2features(self,examples,tokenizer,max_length,label_map):  ### 得到经过tensor的feautres列表
+        features = []
+        for ex in examples:
+            encoded_dict = tokenizer.encode_plus(  ### 这里可以改进，传参
+                                                ex.text,
+                                                add_special_tokens = True,
+                                                max_length=max_length,
+                                                pad_to_max_length = True,
+                                                return_attention_mask = True,
+                                                return_tensors='pt'
+                                                )
+            label_id = t.tensor(label_map[ex.label])
+            features.append(InputFeature(encoded_dict['input_ids'],encoded_dict['attention_mask'],label_id))
+        return features
+
+    def convert_features2dataloader_T_V(self,features,val_batch_size=64,is_train=False,
+                                                      train_batch_size=64,rate=0.8,lable_ids_list=[0,1],class_weight='balanced'):
+        input_ids = []
+        attention_masks = []
+        label_ids = []
+        for fea in features:
+            input_ids.append(fea.input_id)
+            attention_masks.append(fea.attention_mask)
+            label_ids.append(fea.label_id)
+
+        input_ids = t.cat(input_ids, dim=0)  ### Tensor列表 转成 一个 列表Tensor
+        attention_masks = t.cat(attention_masks, dim=0)
+        label_ids = t.tensor(label_ids) ### numpy列表 转成 一个Tensor
+
+        data = TensorDataset(input_ids,attention_masks,label_ids)
+
+        if is_train == False: ## 如果在训练的话，根据比例分割数据集，并用不同的batchsize来创建各自的dataloader对象
+            sampler_V = SequentialSampler(data)
+            dataloader_V = DataLoader(data, sampler=sampler_V, batch_size= val_batch_size)
+            return dataloader_V
+
+        train_size = int(rate * len(data))
+        val_size = len(data) - train_size
+        train_data,val_data = random_split(data,[train_size,val_size])
+
+        sampler_T = RandomSampler(train_data)
+        dataloader_T = DataLoader(train_data, sampler=sampler_T, batch_size=train_batch_size)
+        weight_T = Tool.get_weight_from_dataset(train_data,lable_ids_list,class_weight)
+
+        my_sampler_V = SequentialSampler(val_data)
+        dataloader_V = DataLoader(val_data, sampler=my_sampler_V, batch_size=val_batch_size)
+        weight_V = Tool.get_weight_from_dataset(val_data,lable_ids_list,class_weight)
+
+        return dataloader_T,dataloader_V,weight_T,weight_V
+
+
+
+  
---
+++ finbert/spider.py
@@ -0,0 +1,80 @@
+﻿import requests
+import re
+import time
+import random
+import json
+from lxml import etree
+import re
+import pandas as pd
+
+
+class Spider_For_GUBA(object):
+    
+    def __init__(self):
+        pass
+    
+    def require_reviews(self,startPage,endPage):
+        base_url = 'http://guba.eastmoney.com'
+
+        headers_h = {"user-agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"}
+
+        review_all = []
+
+        for i in range(startPage-1,endPage): ### 仅爬取三面帖子
+            start_url = "http://guba.eastmoney.com/list,zssh000001,99_{}.html".format(i+1)
+            response = requests.get(url=start_url, headers=headers_h)
+            html = response.content.decode("utf-8")
+            html_obj = etree.HTML(html)
+            div_list = html_obj.xpath('//div[@id="articlelistnew"]/div')
+            for div in div_list: ### 遍历每一个帖子
+                if div.xpath("./span/a/@href") == []:
+                    continue
+                sub_url = str(div.xpath("./span/a/@href")[0])
+                url_post = re.findall('\d{9}',sub_url)[0]
+                reviews = self.require_reviews_of_one_blob(url_post)
+                review_all.extend(reviews)
+            print('page {} : completed'.format(i+1))
+        corpus = pd.DataFrame(review_all,columns=['text','date'])
+        return corpus
+            
+    def require_reviews_of_one_blob(self,url_post):  ### 单个帖子的评论采集
+#        time.sleep(random.random()*3)
+        url = 'http://guba.eastmoney.com/interface/GetData.aspx'  ### t可以为任意值，不一定非要是当前时间
+        headers ={
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Content-Length': '121',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Host': 'guba.eastmoney.com',
+            'Origin': 'http://guba.eastmoney.com',
+            'Referer': 'http://guba.eastmoney.com/news,zssh000001,964725926_1.html',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'
+        } ### 来自XHR
+
+        review_all = []
+
+        i=1
+    
+        while True:  ### 前12面
+            data = {"param": "postid={}&sort=1&sorttype=1&p={}&ps=30".format(url_post,i),
+                    "path": "reply/api/Reply/ArticleNewReplyList","env":"2"} ### XHR
+            r = requests.post(url=url, headers=headers,data=data)
+            data_json = json.loads(r.text)
+        
+            if data_json['re'] == None:
+                break
+            
+            for review in data_json['re']:
+                review_all.append([review['reply_text'],review['reply_time']])
+                if len(review['child_replys']) != 0:
+                    for chlid_review in review['child_replys']:
+                        review_all.append([chlid_review['reply_text'],chlid_review['reply_time']])  
+                
+            if data_json['me'] == "暂无更多评论，欢迎发表观点":
+                break   
+        
+            i = i + 1
+        
+        return review_all  
